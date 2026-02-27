9193dd8fedec7ea39b0f466aae878c107128084d
Labham Sharma <git config --global user.email>
2026-02-27 16:21:00 +0530

    feat: initial WebSocket broadcast server implementation

---
+++ server.js
@@ -0,0 +1,24 @@
+import { WebSocketServer,WebSocket } from "ws";
+const wss=new WebSocketServer({port:8080});
+// connection event
+wss.on('connection',(socket,request)=>{
+    const ip=request.socket.remoteAddress;
+    socket.on('message',(rawdata)=>{
+        const message=rawdata.toString();
+        console.log({rawdata});
+        wss.clients.forEach((client)=>{
+            if(client.readyState===WebSocket.OPEN) client.send(`Server Broadcast: ${message}`);
+
+            
+        })
+    })
+
+
+    socket.on('error',(err)=>{
+        console.log(`Error: ${err.message}: ${ip}`);
+    })
+    socket.on('close',()=>{
+        console.log('Client disconnected');
+    })
+})
+console.log("websocket is live on ws://localhost:8080 ")
